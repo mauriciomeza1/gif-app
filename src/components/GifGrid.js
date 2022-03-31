15e6d4d680b47db5ed596441d6e5a42ef4521237
Mauricio <mauriciomeza95@hotmail.com>
2022-03-31 15:17:31 -0600

    Effects added

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,21 +1,16 @@
-import { useEffect, useState } from "react"
-import { getGifs } from "../helpers/getGifs";
 import { GifGridItem } from "./GifGridItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ( {category} ) => {
 
 
-    const [images, setImages] = useState([])
-
-    useEffect( () => {
-        getGifs(category)
-        .then(setImages)
-    }, [ category ])
+    const { data:images, loading } = useFetchGifs( category )
     
     
   return (
     <>
         <h3>{category}</h3>
+        {loading && <p>Loading</p>}
         <div className="card-grid">
             {
                 images.map( img => (
